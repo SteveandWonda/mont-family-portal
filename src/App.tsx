d4f91dea5d023460f997771ae9673dde1c86962b
mont.family <johno@mont.family>
2026-07-21 23:22:50 +0000

    feat: wire ProjectsWidget into dashboard grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider, useAuth } from './auth'
 import Header from './components/Header'
 import LoginPage from './components/LoginPage'
+import ProjectsWidget from './components/ProjectsWidget'
 import FilesWidget from './components/FilesWidget'
 import YNABWidget from './components/YNABWidget'
 import GiteaWidget from './components/GiteaWidget'
@@ -16,6 +17,7 @@ function Dashboard() {
       <Header />
       <main className="flex-1 p-6 max-w-7xl mx-auto w-full">
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+          <ProjectsWidget />
           <CalendarWidget />
           <FilesWidget />
           <YNABWidget />
